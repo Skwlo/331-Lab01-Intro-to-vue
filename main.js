ffe8090fe8138024db4b6b2258cb16c3b2059132
Kangwen Sun <1950580563@qq.com>
2024-10-25 16:20:27 +0800

    10.1
    until task 10.1

---
+++ main.js
@@ -0,0 +1,14 @@
+const { createApp, ref, computed } = Vue
+const app = createApp({
+    setup(){
+        const cart = ref(0)
+        const premium = ref(true)
+        return {
+            cart,
+            premium
+        }
+    }
+})
+app.component('product-display', productDisplay)
+
+app.mount('#app')
